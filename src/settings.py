13b176d4bef5f42dca93589a3bbceaeda04d3457
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-21 23:36:21 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -40,7 +40,7 @@ HOMEPAGE_URL: str = (
 )
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1545749
+PREVIEW_IMAGE_ID: int = 2131285
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/uav-small-object-detection"
